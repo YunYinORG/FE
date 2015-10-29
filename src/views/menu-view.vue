b270ad07bbbce6778b06587b09588ef70322c81e
New Future <new_future@msn.cn>
2015-10-29 17:22:51 +0800

    更新首页和首页地址

--- src/views/menu-view.vue
+++ src/views/menu-view.vue
@@ -1,24 +1,21 @@
 <template>
   <div class="menu-view">
     <div class="text-center">
-      <h1>云印南天</h1>
+      <h2>云印服务</h2>
     </div>
     <div class="text-center">
-      <h3>make a Better campus!</h3>
+      <h5>make a Better campus!</h5>
     </div>
     <div class="text-center">
       <ul class="default-list">
-        <li><a class="button" href="#/print">打印</a></li>
-        <li><a class="button" v-on="click: onOpenFileTaskModal">上传</a></li>
-        <li><a class="button" href="#/share">共享</a></li>
-        <li><a class="button" href="#/book">教材</a></li>
-        <li><a class="button" href="#/file">文件</a></li>
-        <li><a class="button" href="#/user">个人</a></li>
+        <li><a class="button" title="快速打印" href="#/print"><i class="glyphicon glyphicon-print"></i>打印</a></li>
+        <li><a class="button" title="上传文件到服务器上" v-on="click: onOpenFileTaskModal"><i class="glyphicon glyphicon-upload"></i>上传</a></li>
+        <li><a class="button" title="查看和管理我的文件" href="#/file"><i class="glyphicon  glyphicon-folder-close"></i>文件</a></li>
+        <li><a class="button" title="共享的文件" href="#/share"><i class="glyphicon glyphicon-cloud"></i>共享</a></li>
       </ul>
     </div>
   </div>
 </template>
-
 <script>
 module.exports = {
   methods: {
@@ -32,5 +29,4 @@ module.exports = {
     }
   }
 }
-
 </script>
