9f9689ceb3a4ae3c7bcaf31274887ad994a335f4
New Future <new_future@msn.cn>
2015-10-30 00:55:23 +0800

    菜单更新

--- src/views/menu-view.vue
+++ src/views/menu-view.vue
@@ -4,14 +4,14 @@
       <h2>云印服务</h2>
     </div>
     <div class="text-center">
-      <h5>make a Better campus!</h5>
+      <h5>随时随地更方便的校园打印</h5>
     </div>
     <div class="text-center">
       <ul class="default-list">
         <li><a class="button" title="快速打印" href="#/print"><i class="glyphicon glyphicon-print"></i>打印</a></li>
-        <li><a class="button" title="上传文件到服务器上" v-on="click: onOpenFileTaskModal"><i class="fui-upload"></i>上传</a></li>
+        <li><a class="button" title="上传文件到服务器上" v-on="click: onOpenFileTaskModal"><i class="glyphicon glyphicon-cloud-upload"></i>上传</a></li>
         <li><a class="button" title="查看和管理我的文件" href="#/file"><i class="fui-folder"></i>文件</a></li>
-        <li><a class="button" title="共享的文件" href="#/share"><i class="glyphicon glyphicon-cloud"></i>共享</a></li>
+        <li><a class="button" title="共享的文件" href="#/share"><i class="glyphicon glyphicon-globe"></i>共享</a></li>
       </ul>
     </div>
   </div>
