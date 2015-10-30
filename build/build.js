745d98a2331e24a1c7b48796d472404e54416799
New Future <new_future@msn.cn>
2015-10-31 01:08:26 +0800

    同步

--- build/build.js
+++ build/build.js
@@ -10486,7 +10486,7 @@
 	var po = __webpack_require__(92)
 	
 	
-	var baseurl = 'http://api.yunyin.org/'
+	var baseurl = 'http://localhost/index.php/'
 	
 	module.exports = {
 		rest_api: function(options) {
