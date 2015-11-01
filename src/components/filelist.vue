9a7929d31deffee918a477962d9e773c7d5e937b
Andy Jiang <andyjyuan@163.com>
2015-11-01 11:07:03 +0800

    debug alert added

--- src/components/filelist.vue
+++ src/components/filelist.vue
@@ -219,7 +219,7 @@ function loadData(vuemodel) {
       page: vuemodel.displayedPage,
     },  
     opSuccess: function(info) {
-      alert(JSON.stringfy(info))
+      alert(JSON.stringify(info))
       var filedata = info
       for(var i in filedata) {
         filedata[i].checked = false
